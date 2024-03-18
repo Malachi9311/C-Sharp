96438fcdb2fe870dd8ee376979b84e2bc6014fdc
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-18 17:37:46 +0200

    strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -26,18 +26,33 @@ namespace HelloWorld
             // System.Console.WriteLine("float range: {0}", float.MaxValue.ToString("#"));
             // System.Console.WriteLine("-----------------");
 
-            string[] strArray = { "ntuthuko", "nsika", "malachi", "Tass", "Myguy" };
-            System.Console.WriteLine(strArray);
+            // string[] strArray = { "ntuthuko", "nsika", "malachi", "Tass", "Myguy" };
+            // System.Console.WriteLine(strArray);
             // System.Console.WriteLine(strArray[0]);
             // System.Console.WriteLine(strArray.GetType());
             // System.Console.WriteLine(strArray[0].GetType());
 
-            for (int i = 0; i < strArray.Length; i++)
-            {
-                System.Console.Write("{0}:{1}, ", i, strArray[i]);
-            }
-            System.Console.WriteLine();
+            // for (int i = 0; i < strArray.Length; i++)
+            // {
+            //     System.Console.Write("{0}:{1}, ", i, strArray[i]);
+            // }
+            // System.Console.WriteLine();
             System.Console.WriteLine("-----------------");
+
+            // String str = "this is a random string";
+
+            // System.Console.WriteLine("String length: {0}", str.Length);
+            // System.Console.WriteLine("String contains 'is': {0} ", str.Contains("is"));
+            // System.Console.WriteLine("{0}", str.Remove(10, 6));
+            // System.Console.WriteLine("{0}", str.Replace("string", "sentence"));
+
+            object[] ranArr = { "mel", 10, 1.23 };
+
+            for (int i = 0; i < ranArr.Length; i++)
+            {
+                System.Console.Write("{0} ->  ", ranArr[i]);
+                System.Console.WriteLine("{0}", ranArr[i].GetType());
+            }
         }
     }
 }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e18d5b50dcb51f167a9406e377fdbb1121b2327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb05a82af213595c9346f69f8af39c5367d887b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
