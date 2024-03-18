2bb05a82af213595c9346f69f8af39c5367d887b
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-18 16:00:48 +0200

    String Array

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -16,14 +16,27 @@ namespace HelloWorld
             // System.Console.WriteLine($"Hello, {name}");
             System.Console.WriteLine("-----------------");
             // int & long
-            System.Console.WriteLine("byte range: {0}", byte.MaxValue);
-            System.Console.WriteLine("integer range: {0}", int.MaxValue);
-            System.Console.WriteLine("-----------------");
-            System.Console.WriteLine("short range: {0}", short.MaxValue);
-            System.Console.WriteLine("long range: {0}", long.MaxValue);
-            System.Console.WriteLine("-----------------");
-            System.Console.WriteLine("decimal range: {0}", decimal.MaxValue);
-            System.Console.WriteLine("float range: {0}", float.MaxValue.ToString("#"));
+            // System.Console.WriteLine("byte range: {0}", byte.MaxValue);
+            // System.Console.WriteLine("integer range: {0}", int.MaxValue);
+            // System.Console.WriteLine("-----------------");
+            // System.Console.WriteLine("short range: {0}", short.MaxValue);
+            // System.Console.WriteLine("long range: {0}", long.MaxValue);
+            // System.Console.WriteLine("-----------------");
+            // System.Console.WriteLine("decimal range: {0}", decimal.MaxValue);
+            // System.Console.WriteLine("float range: {0}", float.MaxValue.ToString("#"));
+            // System.Console.WriteLine("-----------------");
+
+            string[] strArray = { "ntuthuko", "nsika", "malachi", "Tass", "Myguy" };
+            System.Console.WriteLine(strArray);
+            // System.Console.WriteLine(strArray[0]);
+            // System.Console.WriteLine(strArray.GetType());
+            // System.Console.WriteLine(strArray[0].GetType());
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                System.Console.Write("{0}:{1}, ", i, strArray[i]);
+            }
+            System.Console.WriteLine();
             System.Console.WriteLine("-----------------");
         }
     }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0524d9af2822999db59bb925f2435f9913be7bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e18d5b50dcb51f167a9406e377fdbb1121b2327")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
