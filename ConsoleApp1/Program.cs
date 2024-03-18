58437f64c925cf4bfe1e5d6d48b602701e4fc839
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-18 19:47:55 +0200

    struct & class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,42 @@
-﻿namespace consoleApp1
+﻿using System.Drawing;
+
+namespace consoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double area = ShapeMath.GetArea("Rectangle", 2, 3);
+            // double area = ShapeMath.GetArea("Rectangle", 2, 3);
+            // System.Console.WriteLine("{0}", area);
+
+            Rectangle rect;
+            rect.length = 200;
+            rect.width = 50;
+
+            double area = rect.Area();
             System.Console.WriteLine("{0}", area);
+
+            Rectangle rect1 = new Rectangle(100, 75);
+            System.Console.WriteLine("{0}", rect1.Area().GetType().ToString());
+        }
+
+        struct Rectangle
+        {
+            // Fields / Attributes.
+            public double length;
+            public double width;
+
+            // Constructor
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+            // Method
+            public double Area()
+            {
+                return length * width;
+            }
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f01a0e0544c9737b2f974f77dc210e2fb2a8da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2135d7621c1d67fc37ec868a09072f13ab775491")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
