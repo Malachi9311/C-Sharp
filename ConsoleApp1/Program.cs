2135d7621c1d67fc37ec868a09072f13ab775491
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-18 19:20:09 +0200

    shape class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace consoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = ShapeMath.GetArea("Rectangle", 2, 3);
+            System.Console.WriteLine("{0}", area);
+        }
+    }
+}
