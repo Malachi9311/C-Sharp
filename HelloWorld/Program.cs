276a98e60f485fc9fefe2c45e0821c11dd1b4ca5
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-18 15:25:16 +0200

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            // WriteLine
+            Console.WriteLine("Hello, World!!!");
+            Console.WriteLine("Again");
+
+            // ReadLine
+            Console.Write("Whats Your Name? ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
