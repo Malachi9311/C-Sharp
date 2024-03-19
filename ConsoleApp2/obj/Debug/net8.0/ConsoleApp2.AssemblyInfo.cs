df59683dffe1d4936514b17693790544141ecdb1
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-19 20:23:00 +0200

    mods

--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5c329d6bf9493ba45138de487b403dc05b95f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f77c11f5c19d9114ec19ec4ebe5ae8ee976c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
