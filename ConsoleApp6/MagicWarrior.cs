b79dd8e29c7d229cf7104b274b045296af582c92
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-20 20:28:19 +0200

    Magic warrior class

--- ConsoleApp6/MagicWarrior.cs
+++ ConsoleApp6/MagicWarrior.cs
@@ -4,6 +4,28 @@ namespace ConsoleApp6
     {
         int teleportChance = 0;
 
-        public MagicWarrior() { }
+        CanTelePort teleportType = new CanTelePort();
+
+        public MagicWarrior(string name = "Warrior", double health = 0, double attMax = 0, double blockMax = 0, int teleportChance = 0)
+            : base(name, health, attMax, blockMax)
+        {
+            this.teleportChance = teleportChance;
+        }
+
+        public override double Block()
+        {
+            Random rnd = new Random();
+            int rndDodge = rnd.Next(1, 100);
+
+            if (rndDodge < this.teleportChance)
+            {
+                System.Console.WriteLine($"{Name} {teleportType.teleport()}");
+                return 10000;
+            }
+            else
+            {
+                return base.Block();
+            }
+        }
     }
 }
